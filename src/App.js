8d881430cd53428bb97e35d2414e916bacc84a27
TimIsOverpowered <admin@angelthump.com>
2024-08-06 23:26:46 -0500

    rm manual

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ const Vods = lazy(() => import("./vods/Vods"));
 const Navbar = lazy(() => import("./navbar/navbar"));
 const YoutubeVod = lazy(() => import("./vods/YoutubeVod"));
 const Games = lazy(() => import("./games/Games"));
-const CustomVod = lazy(() => import("./vods/CustomVod"));
 const NotFound = lazy(() => import("./utils/NotFound"));
 const Contests = lazy(() => import("./contests/Contests"));
 const Manage = lazy(() => import("./contests/manage"));
@@ -96,7 +95,6 @@ export default function App() {
               <Route exact path="/live/:vodId" element={<YoutubeVod channel={channel} twitchId={twitchId} type="live" ARCHIVE_API_BASE={ARCHIVE_API_BASE} />} />
               <Route exact path="/youtube/:vodId" element={<YoutubeVod channel={channel} twitchId={twitchId} ARCHIVE_API_BASE={ARCHIVE_API_BASE} />} />
               <Route exact path="/games/:vodId" element={<Games channel={channel} twitchId={twitchId} ARCHIVE_API_BASE={ARCHIVE_API_BASE} />} />
-              <Route exact path="/manual/:vodId" element={<CustomVod channel={channel} twitchId={twitchId} type="manual" ARCHIVE_API_BASE={ARCHIVE_API_BASE} />} />
               <Route
                 exact
                 path="/contests"
